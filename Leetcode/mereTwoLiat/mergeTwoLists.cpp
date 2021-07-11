179d575feb4ecc75a9e8d03645a0ef31c9eb8fbe
SH DU <dushaohe_hit@163.com>
2021-07-11 21:20:37 +0800

    add ListNode Target

---
+++ Leetcode/mereTwoLiat/mergeTwoLists.cpp
@@ -0,0 +1,23 @@
+//
+//  mergeTwoLists.cpp
+//  Leetcode
+//
+//  Created by dsh on 2021/7/11.
+//
+
+#include "mergeTwoLists.hpp"
+
+ListNode* Solution::mergeTwoList(ListNode* l1,ListNode* l2) {
+    if (l1 == nullptr)
+        return l2;
+    else if (l2 == nullptr)
+        return l1;
+    
+    if (l1->val < l2->val) {
+        l1->next = mergeTwoList(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoList(l1, l2->next);
+        return l2;
+    }
+};
---
+++ Leetcode/romaToInt/binaryTree.cpp
@@ -0,0 +1,10 @@
+//
+//  binaryTree.cpp
+//  Leetcode
+//
+//  Created by dsh on 2021/7/11.
+//
+
+#include "binaryTree.hpp"
+
+
