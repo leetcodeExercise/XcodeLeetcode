7db486cb2497a5392310e75f4197dfd3b24d9c79
SH DU <dushaohe_hit@163.com>
2021-09-08 23:40:41 +0800

    找到原因啦

--- Leetcode/SmartPointer/main.cpp
+++ Leetcode/SmartPointer/main.cpp
@@ -14,7 +14,7 @@
 #include "section.hpp"
 
 typedef std::vector<std::shared_ptr<const Section>> TopLevelGraph;
-typedef std::vector<std::shared_ptr<const Action>> ActionGroup;
+typedef std::vector<std::shared_ptr<Action>> ActionGroup;
 typedef std::unordered_map<std::shared_ptr<const Action>, std::shared_ptr<const Section>> ASMap;
 
 int main(int argc, const char* argv[]) {
@@ -23,15 +23,27 @@ int main(int argc, const char* argv[]) {
     ActionGroup actionGroup;
     MotionAction ma1(0), ma2(1), ma3(2);
     DecisionAction da1(3), da2(4);
-    actionGroup.push_back(std::make_shared<const MotionAction>(ma1));
-    actionGroup.push_back(std::make_shared<const MotionAction>(ma2));
-    actionGroup.push_back(std::make_shared<const MotionAction>(ma3));
-    actionGroup.push_back(std::make_shared<const DecisionAction>(da1));
-    actionGroup.push_back(std::make_shared<const DecisionAction>(da2));
-    // Throw error when destroy shared_ptr
-    // std::shared_ptr<const DecisionAction> decisionAction =
-    //     static_cast<std::shared_ptr<const DecisionAction>>(
-    //         static_cast<const DecisionAction*>(actionGroup[0].get()));
+    actionGroup.push_back(std::make_shared<MotionAction>(ma1));
+    actionGroup.push_back(std::make_shared<MotionAction>(ma2));
+    actionGroup.push_back(std::make_shared<MotionAction>(ma3));
+    actionGroup.push_back(std::make_shared<DecisionAction>(da1));
+    actionGroup.push_back(std::make_shared<DecisionAction>(da2));
+    
+    // Do not use get to initialize or assign another smart pointer
+    std::shared_ptr<int> p(new int(42));  // reference count is 1
+    std::cout << "p count: " << p.use_count() << std::endl;
+    int *q = p.get();  // ok
+    std::cout << "p count: " << p.use_count() << std::endl;  // 1
+    {
+        // new block
+        // undefined: two independent shared_ptr point to the same memory
+        (std::shared_ptr<int>(q));
+        std::cout << "p count: " << p.use_count() << std::endl;  // 1
+    }  // block end, q is destroyed, and the memory to which q points is free
+    std::cout << "p count: " << p.use_count() << std::endl;
+    int foo = *p;  // undefined, the memory to which p points is free
+    std::cout << "foo: " << foo << std::endl;  // 0
+
     for (int i = 0; i < actionGroup.size(); i++) {
         switch (actionGroup[i].get()->type()) {
             case Type::Motion: {
